692423ed13cbf07ea388b046bdbadbae1883214e
Agatawaruwu <agatabisnis@gmil.com>
2021-10-04 20:26:48 +0700

    tugas pertemuan 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class contoh1 extends CI_Controller {
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama Saya Agata Waruwu <br>
+        Saya tinggal di daerah kelapa gading jakarta utara <br>
+        Olahraga yang saya sukai adalah badminton"; 
+    }
+}
+
+?>
