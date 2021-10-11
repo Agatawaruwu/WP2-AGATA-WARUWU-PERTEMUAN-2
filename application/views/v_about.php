d84578845377e9e93207bd86be2d10a3d76067a6
Agatawaruwu <agatabisnis@gmil.com>
2021-10-12 06:10:46 +0700

    Tugas pertemuan 4 dan 5

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : agata</li>
+        <li>Nama Belakang : waruwu</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> tanah merah Kelapa Gading, Jakarta Utara</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Nias, Medan, Sumatera Utara</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>musik gitar</li>
+    </ul>
+</section>
